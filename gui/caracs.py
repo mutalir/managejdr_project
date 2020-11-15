dd0531c0ee406dc10afac9249cddfe6a912872d0
XanAstia <xan.astia@live.fr>
2020-11-15 03:28:38 +0100

    - Ajout du GUI pour créer les PNJ - Ajout du GUI secondaire pour afficher les caractéristiques des PNJs - Ajout du script testgui.py pour lancer le gui. - Ajout du stylesheet pour l'apparence d

---
+++ gui/caracs.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'caracs.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(336, 639)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
+        self.gridLayout.setObjectName("gridLayout")
+        self.frame = QtWidgets.QFrame(self.centralwidget)
+        self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame.setObjectName("frame")
+        self.gridLayout_2 = QtWidgets.QGridLayout(self.frame)
+        self.gridLayout_2.setObjectName("gridLayout_2")
+        self.verticalLayout = QtWidgets.QVBoxLayout()
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.label = QtWidgets.QLabel(self.frame)
+        font = QtGui.QFont()
+        font.setPointSize(15)
+        self.label.setFont(font)
+        self.label.setTextFormat(QtCore.Qt.AutoText)
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
+        self.label.setObjectName("label")
+        self.verticalLayout.addWidget(self.label)
+        self.CaracsTable = QtWidgets.QTableView(self.frame)
+        self.CaracsTable.setObjectName("CaracsTable")
+        self.verticalLayout.addWidget(self.CaracsTable)
+        self.gridLayout_2.addLayout(self.verticalLayout, 0, 0, 1, 1)
+        self.gridLayout.addWidget(self.frame, 0, 0, 1, 1)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 336, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label.setText(_translate("MainWindow", "Caractéristiques"))
+
---
+++ testgui.py
@@ -0,0 +1,93 @@
+from gui import Main, caracs
+from pnj import pnj
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class StatsTableModel(QtCore.QAbstractTableModel):
+    #Permet de donner la forme du tableau et comment il doit être rempli.
+    def __init__(self, caracs):
+        super(StatsTableModel, self).__init__()
+
+        self.stats = list(caracs.keys())
+        self._data = [[caracs[key]] for key in self.stats]
+
+    def data(self, index, role):
+        #Donne la manière dont doit être choisi les données du tableau.
+        #index.row donne la ligne actuellement considérée par l'application et index.column, la colonne.
+        #Tous les rôles permettent à l'application de savoir quoi regarder.
+        if role == QtCore.Qt.DisplayRole:
+            # See below for the nested-list data structure.
+            # .row() indexes into the outer list,
+            # .column() indexes into the sub-list
+            return self._data[index.row()][index.column()]
+
+        if role == QtCore.Qt.TextAlignmentRole:
+            return QtCore.Qt.AlignVCenter + QtCore.Qt.AlignHCenter
+
+        if role == QtCore.Qt.BackgroundRole:
+            return QtGui.QColor('black')
+
+        if role == QtCore.Qt.ForegroundRole:
+            return QtGui.QColor('orange')
+
+    def headerData(self, section, orientation, role):
+        # section is the index of the column/row.
+        if role == QtCore.Qt.DisplayRole:
+            if orientation == QtCore.Qt.Vertical:
+                return str(self.stats[section])
+
+    def rowCount(self, index):
+        # The length of the outer list.
+        return len(self._data)
+
+    def columnCount(self, index):
+        # The length of the sub-list (columns)
+        return 1
+
+class CaracsWin(caracs.Ui_MainWindow, QtWidgets.QMainWindow):
+    #c'est la deuxième fenêtre. Le "parent" dans super et init permet d'en faire une fenêtre secondaire par rapport
+    #à la fenêtre principale.
+    def __init__(self, parent, PNJ):
+        super(CaracsWin, self).__init__(parent)
+        self.setupUi(self)
+
+        self.StatsModel = StatsTableModel(PNJ.AfficherCaracs())
+        self.CaracsTable.setModel(self.StatsModel)
+        #Permet de rendre la taille de la première colonne adaptée à ce qu'il y a dedans.
+        header = self.CaracsTable.horizontalHeader()
+        header.setSectionResizeMode(0, QtWidgets.QHeaderView.ResizeToContents)
+
+class MainGUI(Main.Ui_MainWindow, QtWidgets.QMainWindow):
+    def __init__(self):
+        super(MainGUI, self).__init__()
+        self.setupUi(self)
+        #Connecte le bouton à la fonction CreatePNJ.
+        self.CreateButton.clicked.connect(self.CreatePNJ)
+        #Connecte les items de la liste à la fonction DisplayCaracs.
+        self.PNJList.itemClicked.connect(self.DisplayCaracs)
+        self.PNJs = {}
+
+    def CreatePNJ(self):
+        #Créer le PNJ et le garde dans self.PNJs sous forme de dictionnaire
+        if self.PaysanBox.isChecked():
+            paysan = pnj.Pnj('Paysan')
+            self.PNJs[paysan.name] = paysan
+            self.PNJList.addItem(paysan.name)
+
+    def DisplayCaracs(self, name):
+        #Lance la deuxième fenêtre.
+        CARACSWINDOW = CaracsWin(self, self.PNJs[name.text()])
+        CARACSWINDOW.show()
+
+
+
+if __name__ ==  '__main__':
+    app = QtWidgets.QApplication([])
+    qt_app = MainGUI()
+    qt_app.show()
+    # set stylesheet
+    file = QtCore.QFile(":/dark.qss")
+    file.open(QtCore.QFile.ReadOnly | QtCore.QFile.Text)
+    stream = QtCore.QTextStream(file)
+    app.setStyleSheet(stream.readAll())
+    app.setStyle("fusion")
+    app.exec_()
